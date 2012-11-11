8db12a8bfcc29fd1b5ef7ef6d005b45824a4d1db
bernien2s <b.sexton@warwick.ac.uk>
2012-11-11 22:31:33 +0000

    initial commit of agentorange and server

---
+++ runAgent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Usage
+#   sh ./runAgent.sh -file [agent filename]
+#
+
+TACAA_HOME=`dirname $0`
+LIB=${TACAA_HOME}/lib
+CLASSPATH=.
+for i in $( ls ${LIB}/*.jar ); do
+    CLASSPATH=${CLASSPATH}:$i
+done
+
+javac -cp $CLASSPATH AgentOrange.java
+java -server -cp $CLASSPATH edu.umich.eecs.tac.aa.agentware.Main -config config/aw.conf
