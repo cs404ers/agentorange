9c8320a1f732bebf87427a8f4a953ce73f48d393
Timmy TheBall <Timmy@warwickcanoe.co.uk>
2012-11-23 01:09:41 +0000

    accidently moved orange

--- AgentOrange.java
+++ AgentOrange.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.net.*;
 
 
-public class AgentOrange extends Agent{
+public class AgentOrange extends Agent {
     /**
      * Basic simulation information. {@link StartInfo} contains
      * <ul>
@@ -73,213 +73,209 @@ public class AgentOrange extends Agent{
     protected PublisherInfo publisherInfo;
 
     /**
-     * The list contains all of the {@link SalesReport sales report} delivered to the agent.  Each
+     * The list contains all of the {@link SalesReport sales report} delivered to the agent. Each
      * {@link SalesReport sales report} contains the conversions and sales revenue accrued by the agent for each query
      * class during the period.
      */
-    protected Queue<SalesReport> salesReports;
+    protected Queue < SalesReport > salesReports;
 
     /**
-     * The list contains all of the {@link QueryReport query reports} delivered to the agent.  Each
+     * The list contains all of the {@link QueryReport query reports} delivered to the agent. Each
      * {@link QueryReport query report} contains the impressions, clicks, cost, average position, and ad displayed
      * by the agent for each query class during the period as well as the positions and displayed ads of all advertisers
      * during the period for each query class.
      */
-    protected Queue<QueryReport> queryReports;
+    protected Queue < QueryReport > queryReports;
 
     /**
      * List of all the possible queries made available in the {@link RetailCatalog retail catalog}.
      */
-    protected Set<Query> querySpace;
-    
+    protected Set < Query > querySpace;
+
     protected Socket comms;
-    
+
     public AgentOrange() {
-        salesReports = new LinkedList<SalesReport>();
-        queryReports = new LinkedList<QueryReport>();
-        querySpace = new LinkedHashSet<Query>();
+        salesReports = new LinkedList < SalesReport > ();
+        queryReports = new LinkedList < QueryReport > ();
+        querySpace = new LinkedHashSet < Query > ();
     }
 
-	 protected void messageReceived(Message message) {
-	        Transportable content = message.getContent();
+    protected void messageReceived(Message message) {
+        Transportable content = message.getContent();
 
-	        if (content instanceof QueryReport) {
-	            handleQueryReport((QueryReport) content);
-	        } else if (content instanceof SalesReport) {
-	            handleSalesReport((SalesReport) content);
-	        } else if (content instanceof SimulationStatus) {
-	            handleSimulationStatus((SimulationStatus) content);
-	        } else if (content instanceof PublisherInfo) {
-	            handlePublisherInfo((PublisherInfo) content);
-	        } else if (content instanceof SlotInfo) {
-	            handleSlotInfo((SlotInfo) content);
-	        } else if (content instanceof RetailCatalog) {
-	            handleRetailCatalog((RetailCatalog) content);
-	        } else if (content instanceof AdvertiserInfo) {
-	            handleAdvertiserInfo((AdvertiserInfo) content);
-	        } else if (content instanceof StartInfo) {
-	            handleStartInfo((StartInfo) content);
-	        }
-	        
-	        System.out.println("(AgentOrange): Message Received");
-	        
-	    }
+        if (content instanceof QueryReport) {
+            handleQueryReport((QueryReport) content);
+        } else if (content instanceof SalesReport) {
+            handleSalesReport((SalesReport) content);
+        } else if (content instanceof SimulationStatus) {
+            handleSimulationStatus((SimulationStatus) content);
+        } else if (content instanceof PublisherInfo) {
+            handlePublisherInfo((PublisherInfo) content);
+        } else if (content instanceof SlotInfo) {
+            handleSlotInfo((SlotInfo) content);
+        } else if (content instanceof RetailCatalog) {
+            handleRetailCatalog((RetailCatalog) content);
+        } else if (content instanceof AdvertiserInfo) {
+            handleAdvertiserInfo((AdvertiserInfo) content);
+        } else if (content instanceof StartInfo) {
+            handleStartInfo((StartInfo) content);
+        }
 
-	    /**
-	     * Sends a constructed {@link BidBundle} from any updated bids, ads, or spend limits.
-	     */
-	    protected void sendBidAndAds() {
-	        BidBundle bidBundle = new BidBundle();
+        System.out.println("(AgentOrange): Message Received");
 
-	        String publisherAddress = advertiserInfo.getPublisherId();
+    }
 
-	        for(Query query : querySpace) {
-	            // The publisher will interpret a NaN bid as
-	            // a request to persist the prior day's bid
-	            double bid = 1.0;
-	            // bid = [ calculated optimal bid ]
+    /**
+     * Sends a constructed {@link BidBundle} from any updated bids, ads, or spend limits.
+     */
+    protected void sendBidAndAds() {
+        BidBundle bidBundle = new BidBundle();
 
+        String publisherAddress = advertiserInfo.getPublisherId();
 
-	            // The publisher will interpret a null ad as
-	            // a request to persist the prior day's ad
-	            Ad ad = new Ad();
-	            // ad = [ calculated optimal ad ]
+        for (Query query: querySpace) {
+            // The publisher will interpret a NaN bid as
+            // a request to persist the prior day's bid
+            double bid = 1.0;
+            // bid = [ calculated optimal bid ]
 
 
-	            // The publisher will interpret a NaN spend limit as
-	            // a request to persist the prior day's spend limit
-	            double spendLimit = 100;
-	            // spendLimit = [ calculated optimal spend limit ]
+            // The publisher will interpret a null ad as
+            // a request to persist the prior day's ad
+            Ad ad = new Ad();
+            // ad = [ calculated optimal ad ]
 
 
-	            // Set the daily updates to the ad campaigns for this query class
-	            bidBundle.addQuery(query,  bid, ad);
-	            bidBundle.setDailyLimit(query, spendLimit);
-	        }
-
-	        // The publisher will interpret a NaN campaign spend limit as
-	        // a request to persist the prior day's campaign spend limit
-	        double campaignSpendLimit = 10000;
-	        // campaignSpendLimit = [ calculated optimal campaign spend limit ]
+            // The publisher will interpret a NaN spend limit as
+            // a request to persist the prior day's spend limit
+            double spendLimit = 100;
+            // spendLimit = [ calculated optimal spend limit ]
 
 
-	        // Set the daily updates to the campaign spend limit
-	        bidBundle.setCampaignDailySpendLimit(campaignSpendLimit);
+            // Set the daily updates to the ad campaigns for this query class
+            bidBundle.addQuery(query, bid, ad);
+            bidBundle.setDailyLimit(query, spendLimit);
+        }
 
-	        // Send the bid bundle to the publisher
-	        if (publisherAddress != null) {
-	            sendMessage(publisherAddress, bidBundle);
-	        }
-	    }
+        // The publisher will interpret a NaN campaign spend limit as
+        // a request to persist the prior day's campaign spend limit
+        double campaignSpendLimit = 10000;
+        // campaignSpendLimit = [ calculated optimal campaign spend limit ]
 
 
-	    /**
-	     * Processes an incoming query report.
-	     *
-	     * @param queryReport the daily query report.
-	     */
-	    protected void handleQueryReport(QueryReport queryReport) {
-	        queryReports.add(queryReport);
-	    }
+        // Set the daily updates to the campaign spend limit
+        bidBundle.setCampaignDailySpendLimit(campaignSpendLimit);
 
-	    /**
-	     * Processes an incoming sales report.
-	     *
-	     * @param salesReport the daily sales report.
-	     */
-	    protected void handleSalesReport(SalesReport salesReport) {
-	        salesReports.add(salesReport);
-	    }
+        // Send the bid bundle to the publisher
+        if (publisherAddress != null) {
+            sendMessage(publisherAddress, bidBundle);
+        }
+    }
 
-	    /**
-	     * Processes a simulation status notification.  Each simulation day the {@link SimulationStatus simulation status }
-	     * notification is sent after the other daily messages ({@link QueryReport} {@link SalesReport} have been sent.
-	     *
-	     * @param simulationStatus the daily simulation status.
-	     */
-	    protected void handleSimulationStatus(SimulationStatus simulationStatus) {
-	        sendBidAndAds();
-	    }
 
-	    /**
-	     * Processes the publisher information.
-	     * @param publisherInfo the publisher information.
-	     */
-	    protected void handlePublisherInfo(PublisherInfo publisherInfo) {
-	        this.publisherInfo = publisherInfo;
-	    }
+    /**
+     * Processes an incoming query report.
+     *
+     * @param queryReport the daily query report.
+     */
+    protected void handleQueryReport(QueryReport queryReport) {
+        queryReports.add(queryReport);
+    }
 
-	    /**
-	     * Processrs the slot information.
-	     * @param slotInfo the slot information.
-	     */
-	    protected void handleSlotInfo(SlotInfo slotInfo) {
-	        this.slotInfo = slotInfo;
-	    }
+    /**
+     * Processes an incoming sales report.
+     *
+     * @param salesReport the daily sales report.
+     */
+    protected void handleSalesReport(SalesReport salesReport) {
+        salesReports.add(salesReport);
+    }
 
-	    /**
-	     * Processes the retail catalog.
-	     * @param retailCatalog the retail catalog.
-	     */
-	    protected void handleRetailCatalog(RetailCatalog retailCatalog) {
-	        this.retailCatalog = retailCatalog;
+    /**
+     * Processes a simulation status notification. Each simulation day the {@link SimulationStatus simulation status }
+     * notification is sent after the other daily messages ({@link QueryReport} {@link SalesReport} have been sent.
+     *
+     * @param simulationStatus the daily simulation status.
+     */
+    protected void handleSimulationStatus(SimulationStatus simulationStatus) {
+        sendBidAndAds();
+    }
 
-	        // The query space is all the F0, F1, and F2 queries for each product
-	        // The F0 query class
-	        if(retailCatalog.size() > 0) {
-	            querySpace.add(new Query(null, null));
-	        }
+    /**
+     * Processes the publisher information.
+     * @param publisherInfo the publisher information.
+     */
+    protected void handlePublisherInfo(PublisherInfo publisherInfo) {
+        this.publisherInfo = publisherInfo;
+    }
 
-	        for(Product product : retailCatalog) {
-	            // The F1 query classes
-	            // F1 Manufacturer only
-	            querySpace.add(new Query(product.getManufacturer(), null));
-	            // F1 Component only
-	            querySpace.add(new Query(null, product.getComponent()));
+    /**
+     * Processrs the slot information.
+     * @param slotInfo the slot information.
+     */
+    protected void handleSlotInfo(SlotInfo slotInfo) {
+        this.slotInfo = slotInfo;
+    }
 
-	            // The F2 query class
-	            querySpace.add(new Query(product.getManufacturer(), product.getComponent()));
-	        }
-	    }
+    /**
+     * Processes the retail catalog.
+     * @param retailCatalog the retail catalog.
+     */
+    protected void handleRetailCatalog(RetailCatalog retailCatalog) {
+        this.retailCatalog = retailCatalog;
 
-	    /**
-	     * Processes the advertiser information.
-	     * @param advertiserInfo the advertiser information.
-	     */
-	    protected void handleAdvertiserInfo(AdvertiserInfo advertiserInfo) {
-	        this.advertiserInfo = advertiserInfo;
-	    }
+        // The query space is all the F0, F1, and F2 queries for each product
+        // The F0 query class
+        if (retailCatalog.size() > 0) {
+            querySpace.add(new Query(null, null));
+        }
 
-	    /**
-	     * Processes the start information.
-	     * @param startInfo the start information.
-	     */
-	    protected void handleStartInfo(StartInfo startInfo) {
-	        this.startInfo = startInfo;
-	    }
+        for (Product product: retailCatalog) {
+            // The F1 query classes
+            // F1 Manufacturer only
+            querySpace.add(new Query(product.getManufacturer(), null));
+            // F1 Component only
+            querySpace.add(new Query(null, product.getComponent()));
 
-	    /**
-	     * Prepares the agent for a new simulation.
-	     */
-	    protected void simulationSetup() {
-	    	
-	    	try {
-	    	
-	    		comms = new Socket("localhost", 6502);
-	    	
-	    	} catch (IOException e) {
-	    		
-	    		
-	    	}
-	    }
+            // The F2 query class
+            querySpace.add(new Query(product.getManufacturer(), product.getComponent()));
+        }
+    }
 
-	    /**
-	     * Runs any post-processes required for the agent after a simulation ends.
-	     */
-	    protected void simulationFinished() {
+    /**
+     * Processes the advertiser information.
+     * @param advertiserInfo the advertiser information.
+     */
+    protected void handleAdvertiserInfo(AdvertiserInfo advertiserInfo) {
+        this.advertiserInfo = advertiserInfo;
+    }
+
+    /**
+     * Processes the start information.
+     * @param startInfo the start information.
+     */
+    protected void handleStartInfo(StartInfo startInfo) {
+        this.startInfo = startInfo;
+    }
+
+    /**
+     * Prepares the agent for a new simulation.
+     */
+    protected void simulationSetup() {
+
+        try {
+
+            comms = new Socket("localhost", 6502);
+
+        } catch (IOException e) {
+
+
+     
+     }   }
+     
+     	    protected void simulationFinished() {
 	        salesReports.clear();
 	        queryReports.clear();
 	        querySpace.clear();
 	    }
-	}
-
+    }
